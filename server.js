07efbf2d867fd869ea74c6aecee7be2efea99995
rahmona-henry <rahmona.henry@icloud.com>
2017-06-22 23:17:51 +1200

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res){
+  res.send('index.html')
+})
+
+app.listen(3000, function(){
+  console.log('Back to simplicity on port 3000')
+})
